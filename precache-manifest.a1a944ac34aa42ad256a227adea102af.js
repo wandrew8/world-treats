69449cca5766d014346a8d0addd1a3faad6c998f
Andrew Weiss <wandrew8@gmail.com>
2020-10-12 10:33:53 -0700

    Updates

---
+++ precache-manifest.a1a944ac34aa42ad256a227adea102af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42b1a6d9dfaf17a7a280b4d8f905c13",
+    "url": "/world-treats/index.html"
+  },
+  {
+    "revision": "a5e601c691b4ee95c48d",
+    "url": "/world-treats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d00645bc73e3c15cbad",
+    "url": "/world-treats/static/js/2.1a652275.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/world-treats/static/js/2.1a652275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e601c691b4ee95c48d",
+    "url": "/world-treats/static/js/main.9af241d4.chunk.js"
+  },
+  {
+    "revision": "0085cc01b7e124e4c625",
+    "url": "/world-treats/static/js/runtime-main.13e68a35.js"
+  }
+]);
