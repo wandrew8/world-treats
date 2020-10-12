10a7e7ede9d00ae89ba2639a4630ec9c90496280
Andrew Weiss <wandrew8@gmail.com>
2020-10-12 10:30:26 -0700

    initilizes files and adds styled components with global styles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './components/GlobalStyles';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
