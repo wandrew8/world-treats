48df5026484f513ba15c06527619cce323ae8408
Andrew Weiss <wandrew8@gmail.com>
2020-10-12 10:59:11 -0700

    adds react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Product from './pages/Product';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import GlobalStyles from './components/GlobalStyles';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router basename="/world-treats">
       <GlobalStyles />
-      <h1>Hello</h1>
-    </div>
+      <Switch>
+          <Route path="/products/:id">
+            <Product />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
