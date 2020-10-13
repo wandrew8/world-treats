5a506195c14ebee0981a31e42bb46a8023d642ed
Andrew Weiss <wandrew8@gmail.com>
2020-10-13 12:25:48 -0700

    adds cart and cart items component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,103 @@
 import React, { useRef, useEffect, useCallback } from 'react'
 import {useTransition, animated} from 'react-spring'
+import styled from 'styled-components';
+import CartItem from './CartItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 
+const CartContainer = styled.div`
+    width: 75%;
+    min-width: 300px;
+    max-width: 400px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    min-height: 100vh;
+    box-shadow: 0px 0px 5px 10px rgba(0,0,0,0.1);
+    background-color: white;
+    overflow: hidden;
+    button {
+        background: none;
+        height: 35px;
+        width: 35px;
+        transition: 200ms ease-in;
+        cursor: pointer;
+        border: solid lightgray 2px;
+    }
+
+    .cart-heading {
+        display: flex;
+        width: 90%;
+        margin: 0 auto;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .items-container {
+        height: 100%;
+        min-height: 300px;
+        max-height: 50vh;
+        overflow: auto;
+
+    }
+
+    .cart-footer {
+
+    }
+
+    .cartButtons {
+        width: 240px;
+        height: 50px;
+        border-radius: 50px;
+        border: solid blue 2px;
+        display: block;
+        margin: 10px auto;
+        background-color: ${props => props.primary ? "blue" : "white"};
+    }
+
+    .cartCheckout {
+        color: white;
+        background-color: blue;
+    }
+
+    .subtotal {
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+        margin: 0 auto;
+    }
+    .shippingInfo {
+        width: 90%;
+        margin: 0 auto;
+        padding: 10px 0px;
+    }
+
+    .info {
+        color: red;
+        font-size: 1.5rem;
+        margin-right: 10px;
+    }
+`;
+
+const items = [
+    {
+        price: 340,
+        name: "Garlic Potato Chips",
+        amountOrdered: 2,
+        packaging: "One bag",
+        image: "https://www.universalyums.com/wp-content/uploads/2020/07/Devil-Chili-Potato-Chips-1536x1536.jpg"
+    }
+]
 const Cart = ({ showCart, setShowCart }) => {
     const ref = useRef();
     const transitions = useTransition(showCart, null, {
-        from: { opacity: 0, transform: "translateX(-200px)" },
-        enter: { opacity: 1, transform: "translateX(0px)" },
-        leave: { opacity: 0, transform: "translateX(-200px)" }
+        from: { opacity: 0, width: "0%" },
+        enter: { opacity: 1, width: "100%" },
+        leave: { opacity: 0, width: "0%" }
     });
     const handleClick = useCallback(event => {
-        if(showCart && event.target.classList.value !== "cart") {
+        if(showCart && !event.target.classList.value.includes("cart")) {
             setShowCart(false);
         }
       });
@@ -23,7 +111,31 @@ const Cart = ({ showCart, setShowCart }) => {
     
     return (
         transitions.map(({ item, key, props }) => {
-            return item && <animated.div key={key} style={props} className="cart" ref={ref}>Hello</animated.div>
+            return item && <animated.div key={key} style={props} className="cart" ref={ref}><CartContainer className="cart">
+                <div className="cart-heading">
+                    <h2>Your Cart</h2>
+                    <button><FontAwesomeIcon onClick={() => setShowCart(false)} icon={faChevronRight} /></button>
+                </div>
+                <hr/>
+                <div className="items-container cart">
+                    {items.map(item => {
+                        return (
+                            <CartItem item={item} />
+                        )
+                    })}
+                </div>
+                <hr/>
+                <div className="cart-footer cart">
+                    <div className="subtotal"><p>Subtotal:</p><p>$0.00</p></div>
+                    <div className="shippingInfo">
+                        <FontAwesomeIcon className="info" icon={faExclamationCircle}></FontAwesomeIcon>
+                        Spend $50 more for free shipping
+                    </div>
+                    <button className="cartButtons">Keep Shopping</button>
+                    <button className="cartButtons cartCheckout">Checkout Now</button>
+                </div>
+
+            </CartContainer></animated.div>
         }
     )
     )
---
+++ src/components/CartItem.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+
+const Container = styled.div`
+    padding: 10px 0px;
+    hr {
+        width: 90%;
+    }
+`;
+
+const Button = styled.button`
+    background: none;
+    height: 35px;
+    width: 35px;
+    border-radius: 50%;
+    margin: 0px 10px;
+`;
+
+const Item = styled.div`
+    display: grid; 
+    grid-template-columns: 60px 1fr;
+    grid-gap: 20px;
+    align-items: center;
+    width: 90%;
+    margin: 0 auto;
+    h3 {
+        margin: 0;
+    }
+    img {
+        height: 60px;
+        width: 60px;
+        object-fit: cover;
+    }
+    .total {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+`;
+
+const convertUSD = (num) => {
+    const dollars = num / 100;
+    return dollars.toLocaleString("en-US", {style:"currency", currency:"USD"});
+}
+
+const CartItem = ({item}) => {
+    const { price, name, amountOrdered, image, packaging } = item
+    return (
+        <Container>
+            <Item>
+                <div>
+                    <img src={image} alt={name} />
+                </div>
+                <div className="item-description">
+                    <h3>{name}</h3>
+                    <div className="total">
+                        <p>{packaging} {convertUSD(price)}</p>
+                        <div className="addItems">
+                            <Button onClick={(e) => { e.preventDefault()}}><FontAwesomeIcon icon={faMinus}></FontAwesomeIcon></Button>
+                            {amountOrdered}
+                            <Button onClick={(e) => { e.preventDefault()}}><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></Button>
+                        </div>
+                    </div>
+                </div>
+            </Item>
+            <hr />
+        </Container>
+    )
+}
+
+export default CartItem;
